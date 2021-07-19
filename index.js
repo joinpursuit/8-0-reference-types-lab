7a95769af4e53de2c5fa595f49369dd45b0b799e
Billy Dominique <billydominique@pursuit.org>
2021-07-19 14:31:22 -0400

    added psedo code

--- index.js
+++ index.js
@@ -5,12 +5,11 @@
  * @returns {Object[]} The same `stores` array that was inputted.
  */
 function addNewStore(stores, store) {
-  // adding store(object) to the end of stores(array)
-  // 
+  // created variable for stores current state
 let sameStore = stores
-
+// pushed store object to the end of stores array  
 stores.push(store)
-
+// returned store array that was inputted
     return sameStore
 }
 
@@ -21,16 +20,14 @@ stores.push(store)
  * @returns {Object[]} The same `stores` array that was inputted.
  */
 function removeStoreAtPosition(stores, index) {
-
+// created variable for stores current state
   let sameStore = stores
-
+// removed store at the specified index
 stores.splice(index,1)
-
+//returned store array that was inputted
 return sameStore
 
 }
-
-
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
@@ -38,9 +35,9 @@ return sameStore
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
 function duplicateStore(store) {
-
+// created a copy of the store array and assigned it a variable by first converting it into a JSON String and then creating a new object with those cloned key: value pairs and assigning it a new variable 
   let storeCopy = JSON.parse(JSON.stringify(store))
-  
+ // return the cloned store 
   return storeCopy
 
 }
