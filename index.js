13ee5f2b1b15d2c9bdc2f8e9394d328f1ce70e60
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-07-19 16:28:20 -0400

    Completed-References-type-lab

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+ stores.push(store);
+ return stores;
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,26 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  stores.splice(index,1);
+  return stores;
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  let copyStore = {};
+    Object.assign(copyStore, store);
+      copyStore.boardGames = copyStore.boardGames.slice();
+        let myObj = copyStore.address;
+          let copyStoreObj = {};
+        Object.assign(copyStoreObj, myObj);
+      copyStore.address = copyStoreObj;
+    return copyStore;
+}
 
 module.exports = {
   addNewStore,
