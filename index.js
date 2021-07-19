421a2ea401dfd4962173caee34dfce4ed6cca7d9
Billy Dominique <billydominique@pursuit.org>
2021-07-19 14:21:17 -0400

    Passed the test

--- index.js
+++ index.js
@@ -4,7 +4,15 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  // adding store(object) to the end of stores(array)
+  // 
+let sameStore = stores
+
+stores.push(store)
+
+    return sameStore
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +20,30 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+
+  let sameStore = stores
+
+stores.splice(index,1)
+
+return sameStore
+
+}
+
+
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+
+  let storeCopy = JSON.parse(JSON.stringify(store))
+  
+  return storeCopy
+
+}
 
 module.exports = {
   addNewStore,
