6f2bf5a17dfbf3be5e219bf4d89c3d6bb23fe145
Christina Loiacono <loiaconochristina@gmail.com>
2021-07-19 15:17:14 -0400

    added 3 functions

--- index.js
+++ index.js
@@ -4,7 +4,11 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  // modify `stores` array by adding a new store 
+  stores.push(store);
+  return stores;
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +16,24 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  // Removes a store object at the given position.
+  stores.splice(index, 1);
+  return stores;
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  // Creates a duplicate of the `store` object.
+  let newStore = {...store}
+  store.boardGames = [];
+  store.address = {};
+  return newStore;
+}
 
 module.exports = {
   addNewStore,
