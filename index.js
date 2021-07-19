5a92cd0d3a117a7f9ca425f7cb9c84fe17986f7e
salrey <tekdehn@gmail.com>
2021-07-19 14:34:30 -0400

    Add solutions to Reference Lab

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+stores.push(store)
+  return stores 
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,22 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  // Use .splice method to remove a an element (a store at a given position (index). This is modifying the original array (destructive)
+let newStores = stores.splice(index, 1)
+  return stores
+} 
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  // In order to create a DEEP copy of an array, use the JSON method. All the values in the newStore object are disconnected from the original store object.
+let newStore = JSON.parse(JSON.stringify(store))
+  return newStore
+}
 
 module.exports = {
   addNewStore,
