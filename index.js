a4e29452d4b2585ce09fb3b7d3888880f80b49ee
Sara Perkaj <saraperkaj@pursuit.org>
2021-07-20 02:08:07 -0400

    Saved progress for the first day.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+ stores.push(store)
+ return stores 
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,22 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  //process.argv
+  const modifiedStores = stores.splice(index, 1)
+  // const store = stores.shift(index)
+  return stores
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  const modifiedStore = store.copyWithin()
+  return store
+}
 
 module.exports = {
   addNewStore,
