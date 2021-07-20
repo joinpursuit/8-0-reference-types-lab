2a39cb9c79c6e328ffcc45c52f231ae3b2007b90
Theodore Frazier <theo.frazier@icloud.com>
2021-07-19 23:38:02 -0400

    submit

--- index.js
+++ index.js
@@ -4,7 +4,23 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+
+
+function addNewStore(addedStore, store) {
+
+  /**added store, being a reference type of of value, takes on the value of the store object, thus we can simply push added */
+
+  addedStore.push(store)
+  return addedStore;
+
+  /**Adding the new store to the memory of store*/
+  addedstore.push('NewStore');
+
+
+
+}
+
+
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +28,32 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  /*Since we are NOT creating a new array, we will use "splice" insitead of "slice". */
+
+  stores.splice(index, 1);
+  /*since we didn't create a new array, we can retyrn the orginal 'stores' array. */
+  return stores
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) { 
+  /*creating the dup array */
+    let arrCopy = Object.assign({}, store);
+    /*boardgames key - not specifiying an argument in slice allows it to work on difference sized arrays*/
+    arrCopy.boardGames = arrCopy.boardGames.slice();
+    let newObj = arrCopy.address;
+    let storeCopy = Object.assign({}, newObj);
+    arrCopy.address = storeCopy;
+    /*returns the new array */
+    return arrCopy;
+    }
+   
+
 
 module.exports = {
   addNewStore,
