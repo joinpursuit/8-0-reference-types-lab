22ff8412c16eee97c9498eaf4dcd9ffb18c1a1a4
EmaleeSoto <emaleesoto@gmail.com>
2022-01-25 15:03:40 -0500

    completed references lab

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  stores.push(store);
+  return stores;
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,36 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  stores.splice(index, 1);
+  return stores;
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  let newStore = {};
+  for(let key in store){
+    if(Array.isArray(store[key])){
+    newStore.boardGames = store.boardGames.slice();
+    } else {
+      newStore[key] = store[key];
+    }
+  }
+  newStore.address = {};
+  Object.assign(newStore.address, store.address);
+  /**Alternative solution found on samanthaming.com
+   * Object.assign(newStore, store); //this method clones an object without reference, clones store
+   * newStore.boardGames = [...store.boardGames]; //this clones an array without reference, clones store.boardGames
+   * newStore.address = JSON.parse(JSON.stringify(store.address)) //DEEP clones nested object store.address
+    //JSON.stringify converts an Object/value into a JSON string
+  */
+  
+  return newStore;
+}
 
 module.exports = {
   addNewStore,
