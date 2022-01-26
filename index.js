30593ef5bfd8ec6f16190b96caa90c75343a35ca
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-01-25 22:53:14 -0500

    Two out of Three

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 /**
  * Adds a new store to the very end of the list.
  * @param {Object[]]} stores - An array of store objects.
- * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
+ * @param {Object} store - An object representing a single store. 
+ * See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  stores.push(store);
+  
+  return stores;
+}
 
 /**
  * Removes a store object at the given position.
  * @param {Object[]]} stores - An array of store objects.
- * @param {number} index - A number representing the index of the store to be removed from the array.
+ * @param {number} index - A number representing the index of the 
+ * store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  stores.splice(index, 1);
+
+  return stores;
+}
 
 /**
- * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
- * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
- * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
+ * Creates a duplicate of the `store` object. No references should 
+ * be shared between the inputted `store` and the result.
+ * @param {Object} store - An object representing a single store. 
+ * See the instructions for details on its shape.
+ * @returns {Object} The duplicated store object. This should not be 
+ * the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  
+  const newStore = Object.create(store);
+  newStore.boardGames
+  
+   
+
+
+  return store;
+  //     const store = {
+  //   name: "Gamestoria",
+  //   boardGames: [
+  //     "Terra Mystica",
+  //     "Alhambra",
+  //     "Scythe",
+  //     "Carcassonne",
+  //     "Azul",
+  //   ],
+  //   address: {
+  //     street: "42-11 Broadway",
+  //     city: "Astoria",
+  //     state: "NY",
+  //     zip: "11103",
+  //   },
+  // };
+}
 
 module.exports = {
   addNewStore,
