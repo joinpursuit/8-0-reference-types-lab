0a05cf655d9b63219c14d967dbe60feb5abc1ea6
Tom LaTulipe <tlatulipe@gmail.com>
2022-01-27 01:13:28 -0500

    Added some reference notes and formatting changes.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /**
  * Adds a new store to the very end of the list.
  * @param {Object[]]} stores - An array of store objects.
- * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
+ * @param {Object} store - An object representing a single store. See the
+ *  instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
+
 function addNewStore(stores, store) {
   stores.push(store);
   return stores;
@@ -12,21 +14,33 @@ function addNewStore(stores, store) {
 /**
  * Removes a store object at the given position.
  * @param {Object[]} stores - An array of store objects.
- * @param {number} index - A number representing the index of the store to be removed from the array.
+ * @param {number} index - A number representing the index of the store to be 
+ * removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
+
 function removeStoreAtPosition(stores, index) {
   stores.splice(index, 1);
   return stores;
 }
 
 /**
- * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
- * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
- * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
+ * Creates a duplicate of the `store` object. No references should be shared
+ *  between the inputted `store` and the result.
+ * @param {Object} store - An object representing a single store. See the 
+ * instructions for details on its shape.
+ * @returns {Object} The duplicated store object. This should not be the same as 
+ * the store that was inputted.
  */
 
 function duplicateStore(store) {
+  // Todo : Create a loop that can iterate through all keys in an Object,
+  // performing an Object.assign() operation on them. Possible?
+
+  // Object.assign(type, source) => Makes a copy of all values from all
+  // enumerable own properties from one or more source objects to the target. 
+  // Returns the target object
+
   const newStore = Object.assign({}, store);
   newStore.boardGames = Object.assign([], store.boardGames);
   newStore.address = Object.assign({}, store.address);
