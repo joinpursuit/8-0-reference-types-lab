1c856c65a1ee6ea612a533787afd90a8bfff40fe
Tia M😍 <tiam@Tias-Air.fios-router.home>
2022-01-28 21:33:03 -0500

    completed ref lab

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+ stores.push(store)
+return stores
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,17 +15,41 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  let remStore = stores.splice(index, 1)
+  return stores
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  let dupStore = {};
+  dupStore.name = store.name;
+   dupStore.boardGames = Object.assign([], store.boardGames);
+    dupStore.address = Object.assign({}, store.address);
+    return dupStore
+
+
+
+
+  // dupStore.boardGames[0] = store.boardGames[0]
+  // dupStore.boardGames[]
+  // dupStore.address = {};
+  // dupStore.address.street = store.address.street
+  // dupStore.address.city = store.address.city
+  // dupStore.address.state = store.address.state
+  // dupStore.address.zip = store.address.zip
+  // for(var i = 0; i < store.boardGames.length; i++){
+  //   dupStore.boardGames = store.boardGames
+  //   return store
+  }
+  
 
 module.exports = {
   addNewStore,
   removeStoreAtPosition,
   duplicateStore,
-};
+}
