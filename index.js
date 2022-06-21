6cf5e288aa67b06802086a8bb789e0ab1d314502
i-guerrero <isaacguerrero@pursuit.org>
2022-06-20 21:05:35 -0400

    create store functions for lab

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  stores.push(store);
+  return stores;
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,42 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  stores.splice(index, 1);
+  return stores;
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  let storeCopy = {};
+  let boardGamesArrayCopy = [];
+  let addressObjectCopy = {};
+  // Make a copy of the store.boardGames array
+  for (let boardGame of store.boardGames) {
+    boardGamesArrayCopy.push(boardGame);
+  }
+  // Make a copy of the store.address object
+  for (let key in store.address) {
+    addressObjectCopy[key] = store.address[key];
+  }
+
+  // Make a copy of store object
+  storeCopy.name = store.name; // creates a copy of the name value, not a reference to it since strings are primitive
+  storeCopy.boardGames = boardGamesArrayCopy;
+  storeCopy.address = addressObjectCopy;
+
+  // This solution did not work alone because the boardGames array and address object
+  // when assigned to storeCopy, the reference to the same object is passed instead of a copy of the object.
+  // a copy had to be made manually.
+  // for (let key in store) {
+  //   storeCopy[key] = store[key];
+  // }
+  return storeCopy;
+}
 
 module.exports = {
   addNewStore,
