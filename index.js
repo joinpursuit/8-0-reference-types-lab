0dd580aa93afc4d7bc5b1d0475f16f00b3e90b83
ayd823 <aydiaz823@gmail.com>
2022-07-17 08:11:14 -0400

    Reference Types Lab

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /**
  * Adds a new store to the very end of the list.
- * @param {Object[]]} stores - An array of store objects.
+ * @param {Object[]} stores - An array of store objects.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+stores.push(store);
+return stores;
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,23 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  stores.splice(index, 1);
+  return stores;
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  return {
+    name: store.name,
+    boardGames: [...store.boardGames],
+    address: {...store.address},
+  };
+}
 
 module.exports = {
   addNewStore,
