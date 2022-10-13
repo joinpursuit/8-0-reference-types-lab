a8e762f69fa52c35f611aad2c07b4f39477de8c8
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2022-10-13 16:29:04 -0400

    pass removeStoreAtPosition() test

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  */
 function addNewStore(stores, store) {
   stores.push(store)
+  
   return stores
 }
 
@@ -15,7 +16,11 @@ function addNewStore(stores, store) {
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  stores.splice(index, 1)
+
+  return stores
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
