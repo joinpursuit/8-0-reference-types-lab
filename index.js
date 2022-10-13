d3e6d182de404486a97906559386b8d5a6b1d287
YasserS-21 <spotifyspare123@gmail.com>
2022-10-13 15:07:30 -0400

    Revert "Calculator"
    This reverts commit d4b566bb3f4dee5cb789d15a32d74b1e8b1e9987.

--- index.js
+++ index.js
@@ -4,10 +4,7 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {
-  stores.push(store)
-  return stores
-}
+function addNewStore(stores, store) {}
 
 /**
  * Removes a store object at the given position.
@@ -15,24 +12,14 @@ function addNewStore(stores, store) {
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {
-  stores.splice(index, 1)
-  return stores
-}
+function removeStoreAtPosition(stores, index) {}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {
-// let newStore = {
-  //  ...store
- // }
- //let newStore = Object.assign({}, store)
- let newStore = JSON.parse(JSON.stringify(store))
-  return newStore
-}
+function duplicateStore(store) {}
 
 module.exports = {
   addNewStore,
