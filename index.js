cca870639e39703149e91cc55c0fffe556559ed3
Amber King <amber.king9292@gmail.com>
2022-10-16 13:53:13 -0400

    Reference Type Lab Atttempt, 3 out of 11 Test Passed

--- index.js
+++ index.js
@@ -4,7 +4,22 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  const result = [];
+  {
+    stores.push(store)
+
+
+
+  }
+  return stores
+}
+//Brainstorming Attempts---V
+//console.log(stores)
+//let newStores = stores(i) ;
+//for (let i = 0; i < stores.length - 1; i++)
+//Array.prototype.push()
+//push() the new store to the end of the array in the object
 
 /**
  * Removes a store object at the given position.
@@ -12,15 +27,35 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  const result = [];
+  {
+    if (index >= 1) {
+      stores.splice(index)//>first option
+    }
+    return stores
+  }
 
-/**
+
+}
+
+// Brainstorming Attempts --V
+//let removed = stores.splice(2, 0)
+// {
+//stores.splice(); -> this will remove index from a certain spot with return the orignal array with the changes 
+// //for (let i = 0; i <= stores.length - 1; i++); {
+//   stores.splice(i)
+// }
+//stores.splice(index, index.length - 1)//not a passable option;//stores.splice(index, 2)//second option, didn't pass by itself
+
+/** 
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) { }
 
+//duplicating the object w/o having a shallow copy 
 module.exports = {
   addNewStore,
   removeStoreAtPosition,
