0d9a70a9d00bc44ad95c7f7e6a9505cad9d19e25
JenniferEinstein <jennifereinstein@gmail.com>
2022-10-18 11:27:55 -0400

    Last function done! Ready to submit.

--- index.js
+++ index.js
@@ -41,16 +41,40 @@ function removeStoreAtPosition(stores, index) {
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
 function duplicateStore(store) {
-  let objectCopy = {}; //objectCopy will store a copy of the store
+  let storeCopy = {}; //objectCopy will store a copy of the store
   let key;
-  for (key in store) {
-    objectCopy[key] = store[key]; //copies each property to the objectCopy object
+  for (key in store) {     //'in' is an inbuilt operator in JS which is used to check if a particular property exists in an object or not
+    storeCopy[key] = store[key]; //copies each property to the objectCopy object
 }
-  return objectCopy;
+store.boardGames = ["Monopoly","Parcheesi","Funtimes"]
+store.address = {
+  street: "8675 309th Street",
+  city: "Lulav",
+  state: "HI",
+  zip: "09898",
+}
+  return storeCopy;
 } 
 
 
-/** 
+/**
+ const store = {
+      name: "Gamestoria",
+      boardGames: [
+        "Terra Mystica",
+        "Alhambra",
+        "Scythe",
+        "Carcassonne",
+        "Azul",
+      ],
+      address: {
+        street: "42-11 Broadway",
+        city: "Astoria",
+        state: "NY",
+        zip: "11103",
+      },
+    };
+
 let copyStore = {};
   let keys = Object.keys(store);
   for (let i = 0; len = keys.length; i < len, i++) {
@@ -60,11 +84,8 @@ let copyStore = {};
   */
 
 // const sheeps = ['🐑', '🐑'];
-
 // const sheeps2 = sheeps;
-
 // sheeps2.push('🐺');
-
 // console.log(sheeps2);
 // // [ '🐑', '🐑', '🐺' ]
 
