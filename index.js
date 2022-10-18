00c86266278b7ec337e7e8762129545b0934d283
BertCasale <bthebest55@gmail.com>
2022-10-17 21:46:44 -0400

    duplicate store function

--- index.js
+++ index.js
@@ -29,7 +29,27 @@ function removeStoreAtPosition(stores, index) {
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  //create a new object
+  let dupeStore = {}
+  //set its values equal to the original stores values
+  dupeStore.name = store.name
+  //create an array for boardgames
+  dupeStore.boardGames = []
+  //go through the board games array and copy each element
+  for (let i = 0; i < store.boardGames.length; i++){
+    dupeStore.boardGames[i] = store.boardGames[i]
+  }
+  //make an object for address
+  dupeStore.address = {}
+  //copy all the address keys and values
+  dupeStore.address.street = store.address.street
+  dupeStore.address.city = store.address.city
+  dupeStore.address.state = store.address.state
+  dupeStore.address.zip = store.address.zip
+  //return new object
+  return dupeStore
+}
 
 module.exports = {
   addNewStore,
